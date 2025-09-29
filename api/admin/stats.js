f48bca840366479e1fd6b91815d4d21792dfa257
blooprocket-create <blooprocket@gmail.com>
2025-09-29 19:42:58 -0400

    Delete api directory

--- api/admin/stats.js
+++
@@ -1,9 +0,0 @@
-import { query } from '../_utils/db.js';
-import { ok } from '../_utils/responses.js';
-import { requireRole } from '../_utils/roleGuard.js';
-
-export default async function handler(req){
-  const [_, err] = requireRole(req, ['admin']); if (err) return err;
-  const totals = await query("select count(*) users, (select count(*) from products where published=true) published, (select coalesce(sum(total_cents),0) from orders) revenue_cents from users");
-  return ok(totals.rows[0]);
-}
--- api/auth/logout.js
+++
@@ -1,4 +0,0 @@
-import { clearAuthCookie } from '../_utils/jwt.js';
-export default async function handler(){
-  return new Response(JSON.stringify({ ok: true }), { status: 200, headers: { 'content-type': 'application/json', ...clearAuthCookie() } });
-}
