da7f0102deac9c9cf5be798f1d44134a4da1a29c
blooprocket-create <blooprocket@gmail.com>
2025-10-02 02:00:02 -0400

    AP V2.5

---
+++ api/admin/[...path].js
@@ -0,0 +1,12 @@
+﻿import { createRouter } from '../../lib/api/router.js';
+import adminStats from '../../lib/api/admin/stats.js';
+import adminUsers from '../../lib/api/admin/users/index.js';
+import adminUserDetail from '../../lib/api/admin/users/[id].js';
+
+const routes = [
+  { pattern: /^\/api\/admin\/stats$/, methods: { GET: adminStats } },
+  { pattern: /^\/api\/admin\/users$/, methods: { GET: adminUsers, POST: adminUsers } },
+  { pattern: /^\/api\/admin\/users\/([^\/]+)$/, methods: { PATCH: adminUserDetail, DELETE: adminUserDetail }, params: ['id'] },
+];
+
+export default createRouter(routes);
