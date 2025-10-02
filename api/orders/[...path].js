8c1a24db2feb6d2d30fc58ec06817a56c9d6690d
blooprocket-create <blooprocket@gmail.com>
2025-10-02 02:18:40 -0400

    Delete api directory

--- api/orders/[...path].js
+++
@@ -1,8 +0,0 @@
-﻿import { createRouter } from '../../lib/api/router.js';
-import ordersIndex from '../../lib/api/orders/index.js';
-
-const routes = [
-  { pattern: /^\/api\/orders$/, methods: { GET: ordersIndex, POST: ordersIndex } },
-];
-
-export default createRouter(routes);
