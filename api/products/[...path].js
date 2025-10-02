ed27644d8bbe3774c550acd3d17c1118050fb103
blooprocket-create <blooprocket@gmail.com>
2025-10-02 02:12:21 -0400

    AP V2.6

---
+++ api/products/[...path].js
@@ -0,0 +1,10 @@
+﻿import { createRouter } from '../../lib/api/router.js';
+import productsIndex from '../../lib/api/products/index.js';
+import productDetail from '../../lib/api/products/[id].js';
+
+const routes = [
+  { pattern: /^\/api\/products$/, methods: { GET: productsIndex, POST: productsIndex } },
+  { pattern: /^\/api\/products\/([^\/]+)$/, methods: { GET: productDetail, PATCH: productDetail, DELETE: productDetail }, params: ['id'] },
+];
+
+export default createRouter(routes);
---
+++ api/services/[...path].js
@@ -0,0 +1,10 @@
+﻿import { createRouter } from '../../lib/api/router.js';
+import servicesIndex from '../../lib/api/services/index.js';
+import serviceDetail from '../../lib/api/services/[id].js';
+
+const routes = [
+  { pattern: /^\/api\/services$/, methods: { GET: servicesIndex, POST: servicesIndex } },
+  { pattern: /^\/api\/services\/([^\/]+)$/, methods: { GET: serviceDetail, PATCH: serviceDetail, DELETE: serviceDetail, POST: serviceDetail }, params: ['id'] },
+];
+
+export default createRouter(routes);
