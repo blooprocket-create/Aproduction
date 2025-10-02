c550db1d08e455fc188cf4532c6aa7905297b9d4
blooprocket-create <blooprocket@gmail.com>
2025-10-02 02:22:06 -0400

    Delete api/me/[...path].js

--- api/me/[...path].js
+++
@@ -1,8 +0,0 @@
-﻿import { createRouter } from '../../lib/api/router.js';
-import entitlements from '../../lib/api/me/entitlements.js';
-
-const routes = [
-  { pattern: /^\/api\/me\/entitlements$/, methods: { GET: entitlements } },
-];
-
-export default createRouter(routes);
